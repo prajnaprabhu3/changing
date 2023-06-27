20e19e38c85e5b217fe20aabf23de931eadd0744
Prajna <prajnapprabhu3@gmail.com>
2023-06-27 10:28:36 +0530

    feat: toggle theme

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// const toggleButton = document.querySelector(".toggle");
+
+// toggleButton.addEventListener("click", () => {
+//   document.body.classList.toggle("dark-mode");
+// });
+
+function myFunction() {
+  const toggleButton = document.querySelector(".toggle");
+
+  toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+  });
+}
